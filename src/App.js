9917c9285631ac76ea819df215cf38ed9d4fd5f3
vithiya <i.vithiya@gmail.com>
2021-05-09 14:36:21 +0530

    added App file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Profile } from './components/Profile';
+import GreetComponent from './components/Greet';
+import About from './components/About';
+import Status from './components/Status';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render () {
+      return (
+        <div className="App">
+        <Profile name='Bharathi' >
+          <p>Hard working Employee</p>
+        </Profile>
+        <GreetComponent />
+        <About />
+        <Status></Status>
+
+        <Profile name='Vinay' >
+          <button>Start Coding</button>
+          </Profile>
+          <GreetComponent />
+        <About />
+        <Status></Status>
+        </div>
+      );
+    }
 }
-
 export default App;
